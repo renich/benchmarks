92b85cb5cf0c3a7e46c4ba2bf27c327a768f8a0d
Rene Bon Ciric <renich@introdesk.g02.org>
2019-12-11 16:47:48 -0600

    added rust

--- one_million/one_million.cr
+++ one_million/one_million.cr
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
 
-0.upto(1000000) do |number|
+0.upto(1_000_000) do |number|
 	print "Hello, this is iteration number: ", number, "\n"
 end
---
+++ one_million/one_million.rs
@@ -0,0 +1,5 @@
+fn main() {
+    for n in 1..1000000 {
+        println!("Hello, this is iteration number: {}", n);
+    }
+}
