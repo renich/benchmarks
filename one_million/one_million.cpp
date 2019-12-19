eb938881d91bdb3bc180633920b963febf51585e
Rene Bon Ciric <renich@introdesk.g02.org>
2019-12-19 16:09:27 -0600

    adding C++ to the benchmarks

---
+++ one_million/one_million.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+
+	for( n = 0; n < 1000000; n++ ) {
+		cout << "Hello, this is iteration number: " << n << "\n";
+	}
+
+	return 0;
+}
