ea1ae55bbfe8469b9783b0ce39a1bd8d926c2193
Rene Bon Ciric <renich@introdesk.g02.org>
2019-12-19 16:29:19 -0600

    java: better syntax

--- one_million/one_million.java
+++ one_million/one_million.java
@@ -1,8 +1,9 @@
 class OneMillion
 {
-    public static void main(String args[]) {
-		int i = 0;
-		for ( i = 0; i < 1000000; i++ ) {
+    public static void main(String args[])
+	{
+		for ( int i = 0; i < 1000000; i++ )
+		{
         	System.out.println( "Hello, this is iteration number: " + i );
 		}
     }
