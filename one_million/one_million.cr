edadce6524129a3562a78a20fdaca764d44d9641
Renich Bon Ćirić <renich@cloudsigma.com>
2020-04-29 13:05:49 -0500

    crystal: setting STDOUT.flush_on_newline = false
    The bug reference is in the file as a comment.

--- one_million/one_million.cr
+++ one_million/one_million.cr
@@ -1,5 +1,8 @@
 #!/usr/bin/env crystal
 
+# https://github.com/crystal-lang/crystal/pull/8935
+STDOUT.flush_on_newline = false
+
 0.upto(1_000_000) do |number|
 	print "Hello, this is iteration number: ", number, "\n"
 end
